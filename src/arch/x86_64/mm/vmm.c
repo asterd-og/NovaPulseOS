23303517fec3414efbce807628b0c19c985ee737
astrido <connect5554@outlook.com>
2023-10-16 14:47:59 -0300

    VMM Changes

--- src/arch/x86_64/mm/vmm.c
+++ src/arch/x86_64/mm/vmm.c
@@ -7,6 +7,13 @@
 #define Present 1
 #define Writable 2
 
+typedef u64 pdp;
+typedef u64 pd;
+typedef u64 pt;
+typedef u64 page;
+
+pdp pml4[512];
+
 volatile struct limine_kernel_address_request keAddrReq = {
     .id = LIMINE_KERNEL_ADDRESS_REQUEST,
     .revision = 0
@@ -16,6 +23,7 @@ u64 kePhysAddr;
 u64 keVirtAddr;
 
 void VmmInit() {
+    u64* pPml4 = &pml4;
     asm volatile("mov %%cr3, %0" : "=r"(pPml4) :: "memory");
 
     asm volatile("mov %0, %%cr3" :: "a" ((void*)pPml4));
